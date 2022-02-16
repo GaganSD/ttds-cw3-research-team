2bc77b049f35bfeb5775d1c1a2d1506330c5b6b9
倪子骞 <niziqian@pc-138-201.customer.ask4.lan>
2022-02-16 03:17:50 +0000

    v1.3 add comments

--- mongoDB_API.py
+++ mongoDB_API.py
@@ -3,13 +3,16 @@ from pymongo.errors import ConnectionFailure
 import pymongo
 import pandas as pd
 
-db_name = "TTDS_test"
-paper_collec_name = "papers_col_test"
-dataset_collec_name = "dataset_col_test"
+db_name = "TTDS"
+collec_name = dict()
+fields_name = dict()
 
-paper_fields = ['title', '#citations', 'date', 'abstract',
+collec_name["paper"] = "papers_info"
+collec_name["dataset"] = "datasets_info"
+
+fields_name["paper"] = ['title', '#citations', 'date', 'abstract',
                 'text', 'authors', 'keywords']
-dataset_fields = ['title', 'subtitle', 'keywords', 'description', '#downloads', 
+fields_name["dataset"] = ['title', 'subtitle', 'keywords', 'description', '#downloads', 
                 '#views', '#votes', 'owner', 'dataset_slug']
 
 class MongoDBClient():
@@ -26,15 +29,26 @@ class MongoDBClient():
 
     # TODO Avoid repeat data from different source (can be removed later)
 
-    # Insert_dataset_data
+    # Insert_data
+
+    ## Input
     # df: The data to be inserted. Type: pandas.Dataframe
+    # data_type: The type of data. Should either be "paper" or "dataset". Type:string
     # source_identifier: name of the source, e.g. "Kaggle", "arvix". Type:string
     # identifier_field_name: name of the field of identifier in the df, could be the "url", or "title"
     #                   should be unique for each datapoint, better be breif. Type:string
-    def insert_dataset_data(self, df, source_identifier, identifier_field_name):
+    #
+    ## Output(Return)
+    # number of documents successfully inserted
+
+    def insert_data(self, df, data_type ,source_identifier, identifier_field_name):
+        if data_type not in ["paper", "dataset"]:
+            print("type must be \"paper\" or  \"dataset\"")
+            return -1
+
         # check fields
         missing_field = list()
-        for field_name in dataset_fields:
+        for field_name in fields_name[data_type]:
             if field_name not in set(df.columns):
                 missing_field.append(field_name)
 
@@ -49,7 +63,7 @@ class MongoDBClient():
         total_num = df.shape[0]
         error_num = 0
         try:
-            self.client[db_name][dataset_collec_name].insert_many(df.to_dict('records'), ordered=False)
+            self.client[db_name][collec_name[data_type]].insert_many(df.to_dict('records'), ordered=False)
         except errors.BulkWriteError as e:
             error_num = len(e.details['writeErrors'])
             panic_list = list(filter(lambda x: x['code'] != 11000, e.details['writeErrors']))
@@ -77,5 +91,5 @@ if __name__ == "__main__":
     print(dataset_df_.head())
 
     # insert_dataset_data
-    success_num = client.insert_dataset_data(dataset_df_, "kaggle", "dataset_slug")
+    success_num = client.insert_data(dataset_df_, "database", "kaggle", "dataset_slug")
     print(success_num)
