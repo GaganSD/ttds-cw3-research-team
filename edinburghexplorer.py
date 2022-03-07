7995c3df8c4ab1a67f0259ddaea881b55ae85161
GaganSD <gagandevagiri@gmail.com>
2022-03-07 11:29:11 +0000

    fix few bugs

--- edinburghexplorer.py
+++ edinburghexplorer.py
@@ -13,7 +13,7 @@ class Parser:
         self.default_url = "https://www.research.ed.ac.uk/en/datasets/"
         self.url_prefix = "?format=&page="
         self.url_suffix = "&export=xls"
-        self.max_limit = 100
+        self.max_limit = 10
         self.main_dataset = None
 
 
@@ -24,53 +24,57 @@ class Parser:
         else: url = self.default_url
 
         curr_page = 0
+
+        fst = True
+        dataset = None
+        while curr_page <= self.max_limit:
             # to download to the folder
             # _urllib.urlretrieve(final_url, "datasets/ed_re_1.xls")
             # path = os.getcwd() + "\datasets\ed_re_1.xls"
-        
-        while curr_page <= self.max_limit:
-
         # work directly with pandas
-            # try:
+            try:
                 curr_url = url + self.url_prefix + str(curr_page) + self.url_suffix
                 dataset = pd.read_excel(curr_url, engine='xlrd')
 
-                if not self.main_dataset:
+                if fst:
                     self.main_dataset = dataset.copy()
+                    fst = False
                 else:
                     self.main_dataset = pd.concat([self.main_dataset, dataset]) 
                 print("---")
-            # except: 
-            #     print(f"Downloaded dataset info from page 0 to page {curr_page}.")
-            #     # if curr_page == self.max_limit:
-            #     #     self.max_limit += 50
-            #     #     print(f"Updating max limit to download new data! New max_limit = {self.max_limit}")
-            #     break
+                if curr_page == self.max_limit:
+                    self.max_limit += 50
+                    print(f"Max limit reached. max_limit updated to {self.max_limit}")
+            except: 
+                    print(f"Downloaded dataset info from page 0 to page {curr_page}.")
+                    break
+
+            if dataset.empty:
+                print(f"Downloaded dataset info from page 0 to page {curr_page}.")
+                break
+            else:
                 curr_page += 1
 
+        try:
+            self.main_dataset.drop(['Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5'], axis=1, inplace=True)
+        except:
+            print("couldn't drop all default irelevant columns")
 
-        print("Dataset Downloaded.")
         print(self.main_dataset.columns)
-        print("row size", self.main_dataset.size)
-        # except:
-        #     print("ERROR downloading dataset")
-        #     return None
+        print("Number of rows:", self.main_dataset.size)
+
+        self._save_to_csv() ## PART OF CODE THAT SHOULD BE CHANGED TO MONGODB
+
+        return None
+
+    def _save_to_csv(self):
+        self.main_dataset.to_csv('edinburgh_research_datasets_info.csv')
         
+    # def ed_research_print_all(self):
 
-        # db = pd.read_csv(r"", encoding="utf-8")
-
-
-        # try:
-        #     dataset.drop(['Unnamed: 3', 'Unnamed: 4', 'Unnamed: 5'], axis=1, inplace=True)
-        # except:
-        #     print("couldn't drop default irelevant columns")
-
-
-    def ed_research_print_all(self):
-
-        if not self.ed_research_repo_path:
-            print("Update variable 'ed_research_repo_path' with path")
-            return None
+    #     if not self.ed_research_repo_path:
+    #         print("Update variable 'ed_research_repo_path' with path")
+    #         return None
         
         dataset = pd.read_excel(self.ed_research_repo_path)
 
