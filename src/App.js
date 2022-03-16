912ab12f31d997dc5114289099692948cbc7dd3d
GaganSD <gagandevagiri@gmail.com>
2022-03-16 22:22:46 +0000

    prepare for vm

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ function App() {
 
   function SearchFunc() {
     showPageButton.current = true;
-    return fetch('http://127.0.0.1:5000/' + create_url(search, values.current)).then(response => response.json()).then(data => {
+    return fetch('http://34.142.71.148:5000/' + create_url(search, values.current)).then(response => response.json()).then(data => {
       setJsonResults(data);
     });
   }
@@ -130,7 +130,7 @@ function App() {
   function QueryExpansion() {
     
     console.log(create_url(search, values.current));
-    return fetch('http://127.0.0.1:5000/QE/' + search).then(response => response.json()).then(data => {
+    return fetch('http://34.142.71.148:5000/QE/' + search).then(response => response.json()).then(data => {
       setJsonQE(data);
     });
   }
