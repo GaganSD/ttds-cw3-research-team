33bcedcff542044dac1adbeb744e0b811a618229
YutoShibata07 <yuto19990715@gmail.com>
2022-03-23 22:47:57 +0000

    debug

--- core_algorithms/ir_eval/create_inverted_index.sh
+++ core_algorithms/ir_eval/create_inverted_index.sh
@@ -1,9 +1,9 @@
 python core_algorithms/ir_eval/kaggle_dataset_collect.py
 
-FILE= "core_algorithms/ir_eval/datasets.json"
+FILE= "datasets.json"
 if [ -f $FILE ];then
   echo "file exists!"
-  rm core_algorithms/ir_eval/datasets.json
+  rm -f datasets.json
 fi
 wget https://production-media.paperswithcode.com/about/datasets.json.gz
 gzip -d datasets*
@@ -11,7 +11,7 @@ python core_algorithms/ir_eval/paperwithcode_dataset_collect.py
 python core_algorithms/ir_eval/edinburghexplorer.py 
 python core_algorithms/ir_eval/uci_dataset_collect.py
 
-# create inverted index
+create inverted index
 python core_algorithms/ir_eval/index_generator.py --stemming True --remove_stopwords True \
 --local_kaggle_dataset core_algorithms/ir_eval/kaggle_dataset_df_page500.csv \
 --local_paperwithcode_dataset core_algorithms/ir_eval/paperwithcode_df.csv \
