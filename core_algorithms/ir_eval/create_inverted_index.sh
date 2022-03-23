1ed1c0888d378b2265fbff760d25108745032df1
stylianosc07 <37484250+stylianosc07@users.noreply.github.com>
2022-03-23 19:13:35 +0000

    Update create_inverted_index.sh

--- core_algorithms/ir_eval/create_inverted_index.sh
+++ core_algorithms/ir_eval/create_inverted_index.sh
@@ -7,6 +7,6 @@ fi
 wget https://production-media.paperswithcode.com/about/datasets.json.gz
 gzip -d datasets*
 
-paperwithcode_dataset_collect.py
+python paperwithcode_dataset_collect.py
 python index_generator.py --stemming True --remove_stopwords True --local_kaggle_dataset kaggle_dataset_df_page500.csv --local_paperwithcode_dataset paperwithcode_df.csv
 
